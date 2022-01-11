6e1660f3b7cee200f82ea0e49bcc4b466cdb3882
pdarwin <46503478+pdarwin@users.noreply.github.com>
2022-01-11 18:16:47 +0000

    indent

--- src/main/java/exercicioCao/Main.java
+++ src/main/java/exercicioCao/Main.java
@@ -14,7 +14,14 @@ public class Main {
 		animais.add (new Gato ("Tareco", 5, 4, false));
 		animais.add (new Gato ("Tigre", 2, 2.5, true));
 		
-		for (Animal animal :animais) System.out.println("Olá! " + animal.comunicar() + " Eu sou o " + animal.getNome()+ ", tenho " + animal.getIdade() + " anos, " + animal.getPeso() + " quilos, e sou um" + returnEsp(animal) + "!"); //" quilos, e sou um " + animal.getRaca() + "!");
+		for (Animal animal :animais) 
+			{
+				System.out.println("Olá! " + animal.comunicar() +
+						" Eu sou o " + animal.getNome() + 
+						", tenho " + animal.getIdade() + " anos, " 
+						+ animal.getPeso() + " quilos, e sou um" + 
+						returnEsp(animal) + "!"); //" quilos, e sou um " + animal.getRaca() + "!");
+			}
 		//for (Cao cao :caes) System.out.println("Olá! " + cao.comunicar() + " Eu sou o " + cao.getNome()+ ", tenho " + cao.getIdade() + " anos, " + cao.getPeso() +" quilos, e sou um " + cao.getRaca() + "!");
 		//for (Gato gato :gatos) System.out.println("Olá! " +  gato.comunicar() + " Eu sou o " + gato.getNome() +", tenho " + gato.getIdade() + " anos, " + gato.getPeso() +" quilos, e sou um" + returnCacador(gato.isCacador()));
 		
@@ -30,11 +37,15 @@ public class Main {
 	public static String returnEsp (Animal aAnimal)
 	{
 		String sEsp= "";
-		if (aAnimal instanceof Cao) sEsp = ", e sou um " + ((Cao)aAnimal).getRaca(); 
-		else { 
-			if (aAnimal instanceof Gato) sEsp = returnCacador(((Gato)aAnimal).isCacador()); 
+		if (aAnimal instanceof Cao) 
+			{
+				sEsp = ", e sou um " + ((Cao)aAnimal).getRaca(); 
+			}
+		else 
+			{ 
+				if (aAnimal instanceof Gato) sEsp = returnCacador(((Gato)aAnimal).isCacador()); 
 			//else sEsp = "";
-		}
+			}
 		return sEsp;
 	}
 }
