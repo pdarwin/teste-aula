8d61e4eb7a543e20f99d10a99d40e2375f979f37
pdarwin <46503478+pdarwin@users.noreply.github.com>
2022-01-11 17:59:55 +0000

    act
    usando a lista classe geral (animal)

--- src/main/java/exercicioCao/Main.java
+++ src/main/java/exercicioCao/Main.java
@@ -6,25 +6,31 @@ public class Main {
 	
 	public static void main(String[] args)
     { 
-		List<Cao> caes = new ArrayList<Cao>();
-		List<Gato> gatos = new ArrayList<Gato>();
+		List<Animal> animais = new ArrayList<Animal>();		
 		
+		animais.add (new Cao ("Piloto", 5, 35, "Grand Danois"));
+		animais.add (new Cao ("Farrusco", 12, 2.7, "Pequenois"));
 		
-		caes.add (new Cao ("Piloto", 5, 35, "Grand Danois"));
-		caes.add (new Cao ("Farrusco", 12, 2.7, "Pequenois"));
+		animais.add (new Gato ("Tareco", 5, 4, false));
+		animais.add (new Gato ("Tigre", 2, 2.5, true));
 		
-		gatos.add (new Gato ("Tareco", 5, 4, false));
-		gatos.add (new Gato ("Tigre", 2, 2.5, true));
-		
-		for (Cao cao :caes) System.out.println("Olá! " + cao.comunicar() + " Eu sou o " + cao.getNome()+ ", tenho " + cao.getIdade() + " anos, " + cao.getPeso() +" quilos, e sou um " + cao.getRaca() + "!");
-		for (Gato gato :gatos) System.out.println("Olá! " +  gato.comunicar() + " Eu sou o " + gato.getNome() +", tenho " + gato.getIdade() + " anos, " + gato.getPeso() +" quilos, e sou um" + returnCacador(gato.isCacador()));
+		for (Animal animal :animais) System.out.println("Olá! " + animal.comunicar() + " Eu sou o " + animal.getNome()+ ", tenho " + animal.getIdade() + " anos, " + animal.getPeso() + " quilos, e sou um" + returnEsp(animal) + "!"); //" quilos, e sou um " + animal.getRaca() + "!");
+		//for (Cao cao :caes) System.out.println("Olá! " + cao.comunicar() + " Eu sou o " + cao.getNome()+ ", tenho " + cao.getIdade() + " anos, " + cao.getPeso() +" quilos, e sou um " + cao.getRaca() + "!");
+		//for (Gato gato :gatos) System.out.println("Olá! " +  gato.comunicar() + " Eu sou o " + gato.getNome() +", tenho " + gato.getIdade() + " anos, " + gato.getPeso() +" quilos, e sou um" + returnCacador(gato.isCacador()));
 		
     }
 	
 	public static String returnCacador (boolean aCacador)
 	{
 		String sCacador;
-		if (aCacador) sCacador = " excelente caçador!"; else  sCacador = "a desgraça a caçar!";
+		if (aCacador) sCacador = " excelente caçador"; else  sCacador = "a desgraça a caçar";
 		return sCacador;
+	}
+	
+	public static String returnEsp (Animal aAnimal)
+	{
+		String sEsp;
+		if (aAnimal instanceof Cao) sEsp = ", e sou um " + ((Cao)aAnimal).getRaca(); else if (aAnimal instanceof Gato) sEsp = returnCacador(((Gato)aAnimal).isCacador()); else sEsp = "";
+		return sEsp;
 	}
 }
